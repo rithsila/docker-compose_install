e74cbef2ffe4b1726624e3e18af4b6e127080da0
Ev3r4s <74228472+rithsila@users.noreply.github.com>
2024-04-11 16:55:15 +0700

    Update insatll.sh

--- insatll.sh
+++ insatll.sh
@@ -1,47 +1,36 @@
 #!/bin/bash
 
-# Check if the script is running as root
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 
-    exit 1
-fi
-
 # Update package lists and upgrade existing packages
-apt update && apt upgrade -y
-
-# Install prerequisites
-apt-get install -y \
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
     unzip \
-    lsb-release
+    lsb-release \
+    git
 
-# Check if Docker is already installed
-if ! command -v docker &> /dev/null; then
-    # Set up Docker repository
-    mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt update
-    apt-get install -y docker-ce docker-ce-cli containerd.io
-fi
+# Install Docker
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# Check if Docker Compose is already installed
-if ! command -v docker-compose &> /dev/null; then
-    # Install Docker Compose
-    apt-get install -y docker-compose
-fi
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-# Test Docker installation
-docker run hello-world
-
-# Enable and start Docker service
-systemctl enable --now docker
+# Start Docker service
+sudo systemctl enable --now docker
 
 # Check Docker and Docker Compose versions
-docker_version=$(docker --version)
-docker_compose_version=$(docker-compose --version)
+sudo docker version
+sudo docker-compose version
 
-echo "Docker version: $docker_version"
-echo "Docker Compose version: $docker_compose_version"
+# Clone SuiteCRM repository and download SuiteCRM
+git clone https://github.com/jontitmus-code/SuiteCRM8_docker.git /root/SuiteCRM/
+wget https://suitecrm.com/download/147/suite86/563895/suitecrm-8-6-0.zip -P /root/SuiteCRM/www/
+unzip /root/SuiteCRM/www/suitecrm-8-6-0.zip -d /root/SuiteCRM/www/
